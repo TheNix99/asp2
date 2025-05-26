8f261f7c2449b48cc9ceaf2082ac7f72b10ebf7b
TheNix99 <213403686+TheNix99@users.noreply.github.com>
2025-05-26 15:01:13 +0200

    MVC Get

---
+++ MVC/Controllers/SocksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+
+namespace MVC.Controllers;
+
+    public class SocksController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = SocksDataset.GetSocks();
+
+            return View(data);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var data = SocksDataset.GetSocks().Where(x => x.Id == id).FirstOrDefault();
+
+            return View(data);
+        }
+    }
